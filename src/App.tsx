8bdd5a6756db95f1f33f1e18d595248e759b80d5
Bryan141816 <bryanantier7@gmail.com>
2025-04-16 16:42:19 +0800

    add: Page titles

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import VolunteerProfiles from './pages/volunteer_management/volunteer_profiles/V
 import ListOfRAFIDonors from './pages/donations_management/list_of_rafi_donors/ListOfRAFIDonors';
 import FundingProposals from './pages/donations_management/funding_proposals/FundingProposals';
 import ResponseDashboard from './pages/response_dashboard/ResponseDashboard';
+import Page from './components/Pages/Pages';
 
 function App() {
 
@@ -16,12 +17,12 @@ function App() {
         <Navbar />
         <main>
           <Routes>
-            <Route path='/lgu_profiling/map_of_cebu' element={<MapOfCebu/>}/>
-            <Route path='/volunteer_management/track_volunteer_application' element={<TrackVolunteerApplication/>}/>
-            <Route path='/volunteer_management/volunteer_profiles' element={<VolunteerProfiles/>}/>
-            <Route path='/donations_management/list_of_rafi_donors' element={<ListOfRAFIDonors/>}/>
-            <Route path='/donations_management/funding_proposals' element={<FundingProposals/>}/>
-            <Route path='/response_dashboard' element={<ResponseDashboard/>}/>
+            <Route path='/lgu_profiling/map_of_cebu' element={<Page title='IDRIS | Map of Cebu'><MapOfCebu/></Page>}/>
+            <Route path='/volunteer_management/track_volunteer_application' element={<Page title='IDRIS | Track Volunteer Application'><TrackVolunteerApplication/></Page> }/>
+            <Route path='/volunteer_management/volunteer_profiles' element={<Page title='IDRIS | Volunteer Profiles'><VolunteerProfiles/></Page>}/>
+            <Route path='/donations_management/list_of_rafi_donors' element={<Page title='IDRIS | List of RAFI Donors'><ListOfRAFIDonors/></Page>}/>
+            <Route path='/donations_management/funding_proposals' element={<Page title='IDRIS | Funding Proposals'><FundingProposals/></Page>}/>
+            <Route path='/response_dashboard' element={<Page title='IDRIS | Response Dashboard'><ResponseDashboard/></Page>}/>
           </Routes>
         </main>  
       </Router>

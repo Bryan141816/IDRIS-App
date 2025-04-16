ab02816f1789e829d5c90a6e5a7e0ab01942c629
Bryan141816 <bryanantier7@gmail.com>
2025-04-16 17:02:01 +0800

    add: Multiple markers can be placed

--- src/components/MapView/MapView.tsx
+++ src/components/MapView/MapView.tsx
@@ -1,24 +1,31 @@
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
 
+interface LatLng {
+  lat: number;
+  lng: number;
+}
+
 interface MapViewProps{
     center: [number, number];
-    marker: [number, number];
+    markers: LatLng[];
 }
 
 
-const MapView: React.FC<MapViewProps> = ({center, marker}) => {
+const MapView: React.FC<MapViewProps> = ({center, markers}) => {
   return (
     <MapContainer center={center} zoom={13} style={{ height: "100vh", width: "100%" }}>
       <TileLayer
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         attribution='&copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors'
       />
-      <Marker position={marker}>
-        <Popup>
-          A pretty popup! <br /> Easily customizable.
-        </Popup>
-      </Marker>
+      {markers.map((point, index) => (
+        <Marker key={index} position={[point.lat, point.lng]}>
+          <Popup>
+            Marker #{index + 1}
+          </Popup>
+        </Marker>
+      ))}
     </MapContainer>
   );
 };
--- src/pages/lgu_profiling/map_of_cebu/MapOfCebu.tsx
+++ src/pages/lgu_profiling/map_of_cebu/MapOfCebu.tsx
@@ -1,7 +1,14 @@
 import MapView from "../../../components/MapView/MapView";
+
+const markers = [
+    { lat: 10.313924, lng: 123.887082 },
+    { lat: 10.316693, lng: 123.898476 },
+    { lat: 10.322786, lng: 123.910633 },
+  ];
+
 const MapOfCebu = () =>{
     return(
-        <MapView center={[10.313924, 123.887082]} marker={[10.313924, 123.887082]}></MapView>
+        <MapView center={[10.313924, 123.887082]} markers={markers}></MapView>
     );
 }
 export default MapOfCebu

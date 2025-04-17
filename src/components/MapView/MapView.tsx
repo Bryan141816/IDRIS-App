5faf81ff9dcdebd4852ad2d0b453c47de0dc8477
Dayskie <dayagkent09@gmail.com>
2025-04-17 16:42:37 +0800

    Side Bar and explore sa routing sa laing page

--- src/components/MapView/MapView.tsx
+++ src/components/MapView/MapView.tsx
@@ -4,26 +4,33 @@ import 'leaflet/dist/leaflet.css';
 interface LatLng {
   lat: number;
   lng: number;
+  lguName?: string;
 }
 
-interface MapViewProps{
-    center: [number, number];
-    markers: LatLng[];
+interface MapViewProps {
+  center: [number, number];
+  markers: LatLng[];
+  onMarkerClick?: (marker: LatLng) => void;
 }
 
-
-const MapView: React.FC<MapViewProps> = ({center, markers}) => {
+const MapView: React.FC<MapViewProps> = ({ center, markers, onMarkerClick }) => {
   return (
-    <MapContainer center={center} zoom={13} style={{ height: "100vh", width: "100%" }}>
+    <MapContainer center={center} zoom={13} style={{ height: "100%", width: "100%" }}>
       <TileLayer
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         attribution='&copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors'
       />
       {markers.map((point, index) => (
-        <Marker key={index} position={[point.lat, point.lng]}>
-          <Popup>
-            Marker #{index + 1}
-          </Popup>
+        <Marker
+          key={index}
+          position={[point.lat, point.lng]}
+          eventHandlers={{
+            click: () => {
+              onMarkerClick && onMarkerClick(point);
+            },
+          }}
+        >
+          <Popup>{point.lguName || `Marker #${index + 1}`}</Popup>
         </Marker>
       ))}
     </MapContainer>
---
+++ src/pages/lgu_profiling/map_of_cebu/LGUSeeMore.tsx
@@ -0,0 +1,11 @@
+const MapOfCebu = () => {
+  return (
+    <div className="app-container">
+    <h1 style={{ color: 'black', fontSize: '104px', fontWeight: 'bold', textAlign: 'center', justifyContent:'center',textAlignLast:"center" }}>
+        See more
+      </h1>
+    </div>
+  );
+};
+
+export default MapOfCebu;

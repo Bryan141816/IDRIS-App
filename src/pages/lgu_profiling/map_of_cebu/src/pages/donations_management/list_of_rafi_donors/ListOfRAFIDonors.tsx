bce81ded9c4b6078edf2ad6d48bc912314b07be0
Dayskie <dayagkent09@gmail.com>
2025-04-18 22:06:55 +0800

    navigating different markers

--- src/pages/lgu_profiling/map_of_cebu/src/pages/donations_management/list_of_rafi_donors/ListOfRAFIDonors.tsx
+++
@@ -1,6 +0,0 @@
-const ListOfRAFIDonors = () =>{
-    return(
-        <h2>List of Rafi Donors</h2>
-    );
-}
-export default ListOfRAFIDonors
--- src/pages/lgu_profiling/map_of_cebu/src/pages/volunteer_management/volunteer_profiles/VolunteerPofiles.tsx
+++
@@ -1,6 +0,0 @@
-const VolunteerProfiles = () =>{
-    return(
-        <h2>Volunteer Profiles</h2>
-    );
-}
-export default VolunteerProfiles

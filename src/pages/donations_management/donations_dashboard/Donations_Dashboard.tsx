dce194a9c73cb4240f57f101882be98e7331c517
Kimkim787 <alcantaraclement@gmail.com>
2025-04-19 00:02:00 +0800

    90% donation dashboard

--- src/pages/donations_management/donations_dashboard/Donations_Dashboard.tsx
+++ src/pages/donations_management/donations_dashboard/Donations_Dashboard.tsx
@@ -1,4 +1,47 @@
 import './dashboard.scss';
+import DownloadableFile from '../../../components/Downloadable_File'; '../../../components/Downloadable_File';
+import pdf_logo from '../files/pdf-logo.png';
+import PieChart from '../../../components/PieChart';
+import { DonationRecord } from './DonationRecord';
+
+
+const reportsSample = [
+  {
+    "filename": "Annual_Donations_Report.pdf",
+    "fileUrl": "../files/Annual_Donations_Report.pdf",
+  },
+  {
+    "filename": "Monthly_Donations_Report.pdf",
+    "fileUrl": "../files/Monthly_Donations_Report.pdf",
+  },
+]
+
+const PieChartSample = {
+  "labels": ["Site A", "Site B", "Site C"],
+  "datasets": [25, 25, 50],
+  "colors": ["#4B91C7", "#7BC8A4", "#F9D57F"],
+}
+
+const DonationRecordSample = [
+  {
+    "donor": "Clement",
+    "amount": 250000,
+    "site": "Site A",
+    "date": "04/18/2025",
+  },
+  {
+    "donor": "Kent",
+    "amount": 250000,
+    "site": "Site B",
+    "date": "04/18/2020",
+  },
+  {
+    "donor": "Bryan",
+    "amount": 500000,
+    "site": "Site C",
+    "date": "01/01/2000",
+  },
+]
 
 const DonationsDashboard = () => {
 
@@ -10,35 +53,63 @@ const DonationsDashboard = () => {
 
   return (
     <>
-      <h3>Donations Statistics</h3>
-
       <div id="dashboard">
+        <h3>Donations Statistics</h3>
         <div id="donation-statistic">
-          <div className="stat-card large">
-            <p className="title">Overall Donations</p>
-            <p className="stat-data">
-              {new Intl.NumberFormat('en-PH', {
-              style: 'currency',
-              currency: 'PHP',
-              minimumFractionDigits: 0
-              }).format(statistics.overall_donations)}
-            </p>
+          <div id="grid-container">
+            <div className="stat-card large">
+              <p className="title">Overall Donations</p>
+              <p className="stat-data">
+                {new Intl.NumberFormat('en-PH', {
+                style: 'currency',
+                currency: 'PHP',
+                minimumFractionDigits: 0
+                }).format(statistics.overall_donations)}
+              </p>
+            </div>
+            
+            <div className="stat-card">
+              <p className="title">Total Donors</p>
+              <p className="stat-data">{statistics.total_donors}</p>
+            </div>
+            <div className="stat-card">
+              <p className="title">Donor Retention</p>
+              <p className="stat-data">{statistics.retention}%</p>
+          </div>
+          </div>  
+            <div id="transparency-report">
+              <p className="title">Transparency Reports</p>
+              {reportsSample.map((report) => (
+                <DownloadableFile icon={pdf_logo} filename={report.filename} fileUrl={report.fileUrl} className='transparency-report-file' />
+              ))}
           </div>
           
-          <div className="stat-card">
-            <p className="title">Total Donors</p>
-            <p className="stat-data">{statistics.total_donors}</p>
-          </div>
-
-          <div className="stat-card">
-            <p className="title">Donor Retention</p>
-            <p className="stat-data">{statistics.retention}%</p>
-          </div>
         </div>
 
-        <div id="transparency-report">
-          <p className="title">Transparency Reports</p>
-          {/* Add content here later */}
+        <h3>Donations Per Site</h3>
+        <div id="donations-pie-chart">
+          <PieChart 
+            labels={PieChartSample.labels} 
+            backgroundColor={PieChartSample.colors} 
+            data={PieChartSample.datasets}
+            width={300}
+            height={300}
+            className='pie-chart'
+          />
+        </div>
+
+        <h3>Donation Record</h3>
+        <div id="donation-record-container">
+          {DonationRecordSample.map((donation, index) => (
+            <DonationRecord 
+              key={index}
+              donor={donation.donor} 
+              amount={donation.amount} 
+              site={donation.site}
+              date={new Date(donation.date)}
+              className='donation-record'
+            />
+          ))}
         </div>
       </div>
     </>

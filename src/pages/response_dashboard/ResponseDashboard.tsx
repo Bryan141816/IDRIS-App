41a9c978dc3ee664865522818c53ce7145a005e4
Bryan141816 <bryanantier7@gmail.com>
2025-04-20 16:17:06 +0800

    just give json for that table baby hahah

--- src/pages/response_dashboard/ResponseDashboard.tsx
+++ src/pages/response_dashboard/ResponseDashboard.tsx
@@ -1,4 +1,4 @@
-import { TableHead, TableData, TableCell,Cell } from "../../components/TableView/table_view";
+import { TableView, Cell } from "../../components/TableView/table_view";
 import './ResponseDashboard.scss'
 const response_data = {
     table_head: [
@@ -22,32 +22,42 @@ const response_data = {
     table_datas: [
         {
             data: [
-                { type: "Text", text: "March 20, 2025", width: "150px" } as Cell,
-                { type: "Text", text: "EOD Report", width: "250px" } as Cell,
-                { type: "Text", text: "Completed", width: "150px" } as Cell,
-                { type: "Button", text: "View", container_width: "150px", button_width: "120px" } as Cell
+                { type: "Text", font_weight: 500,color: "#000",text: "March 20, 2025", width: "150px" } as Cell,
+                { type: "Text", font_weight: 500,color: "#000",text: "EOD Report", width: "250px" } as Cell,
+                { type: "Text", font_weight: 700,color: "#22A900",text: "Completed", width: "150px" } as Cell,
+                { type: "Button", font_weight: 500, color: "#fff",background_color: "#749AB6",text: "View", container_width: "150px", button_width: "120px" } as Cell
             ]
         },
-
+        {
+            data: [
+                { type: "Text", font_weight: 500,color: "#000",text: "March 20, 2025", width: "150px" } as Cell,
+                { type: "Text", font_weight: 500,color: "#000",text: "EOD Report", width: "250px" } as Cell,
+                { type: "Text", font_weight: 700,color: "#22A900",text: "Completed", width: "150px" } as Cell,
+                { type: "Button", font_weight: 500, color: "#fff",background_color: "#749AB6",text: "View", container_width: "150px", button_width: "120px" } as Cell
+            ]
+        },
+        {
+            data: [
+                { type: "Text", font_weight: 500,color: "#000",text: "March 20, 2025", width: "150px" } as Cell,
+                { type: "Text", font_weight: 500,color: "#000",text: "EOD Report", width: "250px" } as Cell,
+                { type: "Text", font_weight: 700,color: "#22A900",text: "Completed", width: "150px" } as Cell,
+                { type: "Button", font_weight: 500, color: "#fff",background_color: "#749AB6",text: "View", container_width: "150px", button_width: "120px" } as Cell
+            ]
+        },
+        {
+            data: [
+                { type: "Text", font_weight: 500,color: "#000",text: "March 20, 2025", width: "150px" } as Cell,
+                { type: "Text", font_weight: 500,color: "#000",text: "EOD Report", width: "250px" } as Cell,
+                { type: "Text", font_weight: 700,color: "#22A900",text: "Completed", width: "150px" } as Cell,
+                { type: "Button", font_weight: 500, color: "#fff",background_color: "#749AB6",text: "View", container_width: "150px", button_width: "120px" } as Cell
+            ]
+        },
     ]
 }
 
 const ResponseDashboard = () =>{
     return(
-        <div id="table-container">
-            <TableHead>
-                {response_data.table_head.map((header,index) => (
-                    <h3 key={index} style={{width: header.width}}>{header.text}</h3>
-                ))}
-            </TableHead>
-            {response_data.table_datas.map((row, rowIndex)=>(
-                <TableData key={rowIndex}>
-                    {row.data.map((cellValue, cellIndex) => (
-                        <TableCell cell={cellValue}></TableCell>
-                    ))}
-                </TableData>
-            ))}
-        </div>
+        <TableView tableJSON={response_data}></TableView>
     );
 }
 export default ResponseDashboard

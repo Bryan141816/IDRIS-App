3d5f9d3b412b7d940e56bd7fc2adf53607864548
Kimkim787 <alcantaraclement@gmail.com>
2025-05-18 13:55:27 +0800

    add: modals

--- src/pages/donations_management/list_of_rafi_donors/Modals.tsx
+++ src/pages/donations_management/list_of_rafi_donors/Modals.tsx
@@ -1,7 +1,5 @@
 import React, { ReactNode } from 'react';
 import style from './modal.module.scss';
-import Search from '../../../components/Public/Search';
-import { useState } from 'react';
 
 type ModalProps = {
   isOpen: boolean;
@@ -10,19 +8,13 @@ type ModalProps = {
 };
 
 export const Modal: React.FC<ModalProps> = ({ isOpen, onClose, children }) => {
-  const [searched, searchState] = useState("");
-
   if (!isOpen) return null;
 
   return (
     <div className={style.modalOverlay}>
       <div className={style.modalContent}>
         <button className={style.closeBtn} onClick={onClose}>×</button>
-        <h3>Add New Donor</h3>
-        <div className={style.formContainer}>
-          <Search placeholder="Search Donor" value={searched} onChange={searchState} />
-
-        </div>
+        {children}
       </div>
     </div>
   );

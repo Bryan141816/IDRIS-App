81ba629feba3c69f9f023cb98b96191b3c41b4f5
Kimkim787 <alcantaraclement@gmail.com>
2025-05-18 21:08:26 +0800

    added: login & logout

---
+++ src/pages/finance&admin/finance_admin.tsx
@@ -0,0 +1,12 @@
+
+
+const FinanceAdmin = () => {
+  return (
+    <div>
+      <h1>Finance and Admin</h1>
+      <p>This is the Finance and Admin page.</p>
+    </div>
+  );
+}
+
+export default FinanceAdmin;
---
+++ src/pages/procurement_inventory/procurement_inventory.tsx
@@ -0,0 +1,12 @@
+
+
+const ProcurementInventory = () => {
+  return (
+    <div>
+      <h1>Procurement & Inventory</h1>
+      <p>This is the Procurement and Inventory page.</p>
+    </div>
+  );
+}
+
+export default ProcurementInventory;

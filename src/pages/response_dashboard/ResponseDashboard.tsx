fb1ae7515c01878d5044613c1e50b2d9b65b24ed
Bryan141816 <bryanantier7@gmail.com>
2025-05-31 11:57:33 +0800

    Cleanup

--- src/pages/response_dashboard/ResponseDashboard.tsx
+++ src/pages/response_dashboard/ResponseDashboard.tsx
@@ -1,7 +1,9 @@
 import { Link } from 'react-router-dom';
-import './ReponseDashboard.scss'
+import './ResponseDashboard.scss'
 import { useUserRoleContext } from '../../UserRoleContext';
 import MapView from '../../components/MapView/MapView';
+import { TableView, Cell } from "../../components/TableView/table_view";
+
 import {
     Chart as ChartJS,
     ArcElement,
@@ -31,6 +33,56 @@ ChartJS.register(
   
 );
   
+const response_data = {
+    table_head: [
+        {
+            text: "Date",
+            width: "150px",
+        },
+        {
+            text: "Report Type",
+            width: "250px",
+        },
+        {
+            text: "Status",
+            width: "150px",
+        },
+    ],
+    table_datas: [
+        {
+            data: [
+                { type: "Text", font_weight: 500,color: "#000",text: "March 20, 2025", width: "150px" } as Cell,
+                { type: "Text", font_weight: 500,color: "#000",text: "EOD Report", width: "250px" } as Cell,
+                { type: "Text", font_weight: 700,color: "#22A900",text: "Completed", width: "150px" } as Cell,
+
+            ]
+        },
+        {
+            data: [
+                { type: "Text", font_weight: 500,color: "#000",text: "March 20, 2025", width: "150px" } as Cell,
+                { type: "Text", font_weight: 500,color: "#000",text: "EOD Report", width: "250px" } as Cell,
+                { type: "Text", font_weight: 700,color: "#22A900",text: "Completed", width: "150px" } as Cell,
+
+            ]
+        },
+        {
+            data: [
+                { type: "Text", font_weight: 500,color: "#000",text: "March 20, 2025", width: "150px" } as Cell,
+                { type: "Text", font_weight: 500,color: "#000",text: "EOD Report", width: "250px" } as Cell,
+                { type: "Text", font_weight: 700,color: "#22A900",text: "Completed", width: "150px" } as Cell,
+
+            ]
+        },
+        {
+            data: [
+                { type: "Text", font_weight: 500,color: "#000",text: "March 20, 2025", width: "150px" } as Cell,
+                { type: "Text", font_weight: 500,color: "#000",text: "EOD Report", width: "250px" } as Cell,
+                { type: "Text", font_weight: 700,color: "#22A900",text: "Completed", width: "150px" } as Cell,
+            ]
+        },
+    ]
+}
+
 
 const data = {
     labels: ['Cash', 'In-kind', 'Services'],
@@ -48,7 +100,7 @@ const data = {
     responsive: true,
     plugins: {
       legend: {
-        position: 'right' as const, // 👈 fix here
+        position: 'bottom' as const, // 👈 fix here
       },
     },
     cutout: '80%', // or a pixel value like '100px'
@@ -176,91 +228,86 @@ const ResponseDashboard = () => {
     const { userRole } = useUserRoleContext();
 
     return(
-        <div id="reponse_container">
-            <h3>Response Dashboard</h3>
-            <div className='sub-items'>
-                <div className='sub-item-info'>
-                    <h3>Reports</h3>
-                    { userRole == "operations admin" && <Link to="/response_dashboard/report_list" className='manage-button' >Manage</Link> }
+        <div className='response_container'>
+          <h3>Response Dashboard</h3>
+          <div className='response-content'>
+            <div style={{display: 'grid',gridTemplateColumns: 'repeat(4, 1fr)', gap: '15px'}}>
+                <h3 style={{gridColumn: 'span 2'}}>Reports</h3>
+                <div className='horizontal-container space-between-container' style={{gridColumn: 'span 2'}}>
+                  <h3>Recent Reports</h3>
+                  { userRole == "operations admin" && <Link to="/response_dashboard/report_list" className='manage-button'>Manage</Link>}
                 </div>
-                <div className='sub-items-grid' style={{gridTemplateColumns: 'repeat(3, 1fr)'}}>
-                    <div className='sub-item-content-big-data'>
-                        <h1>Monthly Report Count</h1>
-                        <span>5,000</span>
-                    </div>
-                    <div className='sub-item-content-big-data'>
-                        <h1>Completed Reports</h1>
-                        <span>2,000</span>
-                    </div>
-                    <div className='sub-item-content-big-data'>
-                        <h1>Started Reports</h1>
-                        <span>3,000</span>
-                    </div>
+                <div className='sub-item-content-big-data' style={{gridColumn: 'span 2'}}>
+                    <h1>Monthly Report Count</h1>
+                    <span>5,000</span>
+                </div>
+                <div className='recent-report'>
+                  <TableView tableJSON={response_data} onClickCallback={()=>{}} ></TableView>
+                </div>
+                    <div className='sub-item-content-big-data' >
+                    <h1>Completed Reports</h1>
+                    <span>3,000</span>
+                </div>
+                <div className='sub-item-content-big-data'>
+                    <h1>Started Reports</h1>
+                    <span>2,000</span>
                 </div>
             </div>
-            <div className='sub-items'>
-                <div className='sub-items-grid' style={{gridTemplateColumns: 'repeat(3, 1fr)'}}>
-                    <h3 style={{gridColumn: 'span 2'}}>Response and Demand Map</h3>
-                    <h3>Modality Distribution</h3>
-                    <div className='sub-item-grid-content' style={{gridColumn: 'span 2'}}>
-                        <form action="" className='view-more-container'>
-                            <button className='view-more'>View More</button>
-                        </form>
-                        <MapView
-                            center={[10.313924, 123.887082]}
-                            markers={markers}
-                            onMarkerClick={()=>{}}
-                            />
-                    </div>
-                    <div className='sub-item-grid-content' >
-                        <form action="" className='view-more-container'>
-                            <button className='view-more'>View More</button>
-                        </form>
-                        <div style={{display: 'flex', padding: '15px', height: '100%', width: '100%',justifyContent: 'center', alignItems:'center'}}>
-                            <Doughnut data={data} options={options}/>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div className='sub-items'>
-                <div className='sub-items-grid' style={{gridTemplateColumns: 'repeat(2, 1fr)'}}>
-                    <div className='sub-item-info' style={{gridColumn: 'span 2'}}>
-                        <h3>Budget</h3>
-                        { userRole == "operations admin" && <Link to="/response_dashboard/report_list" className='manage-button'>Manage</Link>}
-                    </div>
-                    <div className='sub-item-grid-content' style={{ aspectRatio: '2/1', padding: '15px' }}>
-                        <Line data={lineChartData} options={lineChartOptions} />
-                    </div>
-                    <div className='sub-item-grid-content' style={{ aspectRatio: '2/1', padding: '15px' }}>
-                        <Bar data={barChartData} options={barChartOptions} />
-                    </div>
-
-                </div>
-            </div>
-            <div className='sub-items'>
-              <div className='sub-items-grid' style={{ gridTemplateColumns: 'repeat(2, 1fr)' }}>
-                <div className='sub-item-info' style={{ gridColumn: 'span 2' }}>
-                  <h3>In-Kind Monitoring</h3>
-                  <Link to="/response_dashboard/report_list" className='manage-button'>Manage</Link>
-                </div>
-                <div className='sub-item-content-big-data'>
-                  <h1>Relief Packs Available for Distribution</h1>
-                  <span>5,000</span>
-                </div>
-                <div className='sub-item-content-big-data'>
-                  <h1>Relief Packs Currently in Transit</h1>
-                  <span>2,000</span>
-                </div>
-                <div className='sub-item-content-big-data'>
-                  <h1>Total Relief Packs Already Distributed</h1>
-                  <span>10,000</span>
-                </div>
-                <div className='sub-item-content-big-data'>
-                  <h1>Remaining Days for Distribution Completion</h1>
-                  <span>7 Days</span>
+            <div style={{display: 'grid',gridTemplateColumns: 'repeat(4, 1fr)', gap: '15px'}}>
+              <div className='horizontal-container space-between-container'>
+                <h3>Demand and Response Map</h3>
+                { userRole == "operations admin" && <Link to="/response_dashboard/report_list" className='manage-button'>Manage</Link>}
+              </div>
+              <div className='horizontal-container space-between-container'>
+                <h3>Modality Distribution</h3>
+                { userRole == "operations admin" && <Link to="/response_dashboard/report_list" className='manage-button'>Manage</Link>}
+              </div>
+              <div className='horizontal-container space-between-container' style={{gridColumn: 'span 2'}}>
+                <h3>In-Kind Monitoring</h3>
+                { userRole == "operations admin" && <Link to="/response_dashboard/report_list" className='manage-button'>Manage</Link>}
+              </div>
+              <div  style={{ gridRow: 'span 4'}} className='bordered-sub-item'>
+                <MapView
+                         center={[10.313924, 123.887082]}
+                         markers={markers}
+                         onMarkerClick={()=>{}}
+                         />
+              </div>
+              <div  style={{ gridRow: 'span 4', backgroundColor: 'white'}} className='bordered-sub-item'>
+                <div style={{display: 'flex', padding: '15px', height: '100%', width: '100%',justifyContent: 'center', alignItems:'center'}}>
+                    <Doughnut data={data} options={options}/>
                 </div>
               </div>
+               <div className='sub-item-content-big-data-inverted' style={{gridColumn: 'span 2'}}>
+                 <h1>Relief Packs Available for Distribution</h1>
+                 <span>5,000</span>
+               </div>
+               <div className='sub-item-content-big-data-inverted' style={{gridColumn: 'span 2'}}>
+                 <h1>Relief Packs Currently in Transit</h1>
+                 <span>2,000</span>
+               </div>
+               <div className='sub-item-content-big-data-inverted' style={{gridColumn: 'span 2'}}>
+                 <h1>Total Relief Packs Already Distributed</h1>
+                 <span>10,000</span>
+               </div>
+               <div className='sub-item-content-big-data-inverted' style={{gridColumn: 'span 2'}}>
+                 <h1>Remaining Days for Distribution Completion</h1>
+                 <span>7 Days</span>
+               </div>
             </div>
+            <div style={{display: 'grid',gridTemplateColumns: 'repeat(2, 1fr)', gap: '15px', paddingBottom: '30px'}}>
+              <div className='horizontal-container space-between-container' style={{gridColumn: 'span 2'}}>
+                <h3>Budget</h3>
+                { userRole == "operations admin" && <Link to="/response_dashboard/report_list" className='manage-button'>Manage</Link>}
+              </div>
+              <div className='bordered-sub-item' style={{ padding: '15px' }}>
+                  <Line data={lineChartData} options={lineChartOptions} />
+              </div>
+              <div className='bordered-sub-item' style={{ padding: '15px' }}>
+                  <Bar data={barChartData} options={barChartOptions} />
+              </div>
+            </div>
+          </div>
         </div>
     );
 };

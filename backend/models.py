ea2ffb976bdd918618c4ddb786412c3d998052b9
Bryan141816 <bryanantier7@gmail.com>
2025-06-07 11:38:54 +0800

    add: Database migration tool and add report feature

--- backend/models.py
+++ backend/models.py
@@ -1,7 +1,9 @@
 # models.py
+from datetime import timezone
 from enum import unique
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, DateTime
 from sqlalchemy.types import JSON
+from sqlalchemy.sql import func
 from database import Base  # <-- import Base here
 
 class User(Base):
@@ -13,3 +15,11 @@ class User(Base):
     hashed_password = Column(String)
     user_type = Column(String)
     roles = Column(JSON, default=[])
+
+class ResponseReport(Base):
+    __tablename__ = "response_reports"
+
+    id = Column(Integer, primary_key= True, index = True)
+    date_time = Column(DateTime(timezone= True), server_default= func.now(), nullable = False)
+    report_type = Column(String(255), nullable=False)
+    status = Column(String(50), nullable=False)

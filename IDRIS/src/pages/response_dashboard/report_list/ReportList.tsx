2b8c503743d472944fc77b31157c9976ce18d9ff
Bryan141816 <bryanantier7@gmail.com>
2025-06-07 11:38:54 +0800

    add: Database migration tool and add report feature

--- IDRIS/src/pages/response_dashboard/report_list/ReportList.tsx
+++ IDRIS/src/pages/response_dashboard/report_list/ReportList.tsx
@@ -1,37 +1,95 @@
 import {
   TableView,
-  Cell,
   TableReponse,
 } from "../../../components/TableView/table_view";
 import "./ReportList.scss";
 import { Modal } from "../../../components/Page_Furniture/Modals";
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
-import { getReportList } from "../../../API_Handler/reponse_dashboard_report_list";
+import {
+  getReportList,
+  addResponseReport,
+} from "../../../API_Handler/reponse_dashboard_report_list";
 
 const ReportList = () => {
   const [response_data, setResposeData] = useState<TableReponse | null>(null);
-  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [isViewModalOpen, setIsViewModalOpen] = useState(false);
+  const [isAddModalOpen, setAddModalOpen] = useState(false);
+  const [reportType, setReportType] = useState("");
 
-  const openModal = () => setIsModalOpen(true);
-  const closeModal = () => setIsModalOpen(false);
-  useEffect(() => {
-    async function fetchData() {
-      try {
-        const response = await getReportList();
-        setResposeData(response);
-        console.log(response_data);
-      } catch (error) {
-        console.error(error);
-      }
+  const openViewModal = () => setIsViewModalOpen(true);
+  const closeViewModal = () => setIsViewModalOpen(false);
+
+  const openAddModal = () => setAddModalOpen(true);
+  const closeAddModal = () => setAddModalOpen(false);
+
+  async function fetchData() {
+    try {
+      const response = await getReportList();
+      setResposeData(response);
+      console.log(response_data);
+    } catch (error) {
+      console.error(error);
     }
+  }
+
+  useEffect(() => {
     fetchData();
   }, []);
 
+  const handleReportTypeChange = (
+    event: React.ChangeEvent<HTMLSelectElement>,
+  ) => {
+    setReportType(event.target.value);
+  };
+
+  const handleAddReportSubmit = async () => {
+    try {
+      const response = await addResponseReport(reportType);
+      console.log("Report added: ", response);
+      closeAddModal();
+      await fetchData();
+    } catch (error) {
+      console.error("Failed to add report: ", error);
+    }
+  };
+
   return (
     <div className="report-container">
-      <Modal isOpen={isModalOpen} onClose={closeModal}>
+      <Modal isOpen={isAddModalOpen} onClose={closeAddModal}>
+        <div className="modal-container">
+          <div className="horizontal-container">
+            <span className="details-title">Create Report</span>
+          </div>
+          <div className="horizontal-container">
+            <span className="item-details-identifier">Report Type:</span>
+            <select value={reportType} onChange={handleReportTypeChange}>
+              <option value="EDO Report">EOD Report</option>
+              <option value="Budget Report">Budget Report</option>
+              <option value="Distribution Report">Distribution Report</option>
+              <option value="Demand Assessment">Demand Assessment</option>
+              <option value="Modality Report">Modality Report</option>
+              <option value="In-Kind Monitoring">In-Kind Monitoring</option>
+            </select>
+          </div>
+          <div className="action-button">
+            <button
+              style={{ backgroundColor: "#749AB6" }}
+              onClick={handleAddReportSubmit}
+            >
+              Add
+            </button>
+            <button
+              style={{ backgroundColor: "#F84B4D" }}
+              onClick={closeAddModal}
+            >
+              Cancel
+            </button>
+          </div>
+        </div>
+      </Modal>
+      <Modal isOpen={isViewModalOpen} onClose={closeViewModal}>
         <div className="modal-container">
           <div className="horizontal-container">
             <span className="details-title">Details</span>
@@ -49,10 +107,16 @@ const ReportList = () => {
             <span>March 20, 2025</span>
           </div>
           <div className="action-button">
-            <button style={{ backgroundColor: "#749AB6" }} onClick={closeModal}>
+            <button
+              style={{ backgroundColor: "#749AB6" }}
+              onClick={closeViewModal}
+            >
               Ok
             </button>
-            <button style={{ backgroundColor: "#F84B4D" }} onClick={closeModal}>
+            <button
+              style={{ backgroundColor: "#F84B4D" }}
+              onClick={closeViewModal}
+            >
               Cancel
             </button>
           </div>
@@ -66,11 +130,11 @@ const ReportList = () => {
         <div className="table-actions">
           <input type="text" placeholder="Search report"></input>
           <button>Search</button>
-          <button>+ Add Report</button>
+          <button onClick={openAddModal}>+ Add Report</button>
         </div>
       </div>
       {response_data ? (
-        <TableView tableJSON={response_data} onClickCallback={openModal} />
+        <TableView tableJSON={response_data} onClickCallback={openViewModal} />
       ) : (
         <div>Loading data...</div>
       )}

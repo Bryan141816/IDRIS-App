1ef3a96a14d14814b159640bba3a5ed4a062ff9f
Bryan141816 <bryanantier7@gmail.com>
2025-07-26 23:13:46 +0800

    ref: added visualizer for debuging

--- IDRIS/vite.config.ts
+++ IDRIS/vite.config.ts
@@ -1,8 +1,9 @@
-import { defineConfig } from 'vite'
+import { defineConfig, type PluginOption } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import { visualizer } from "rollup-plugin-visualizer";
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), visualizer({ open: true, filename: 'stats.html', template: 'flamegraph' }) as PluginOption],
   // css: {
   //   preprocessorOptions: {
   //     scss: {

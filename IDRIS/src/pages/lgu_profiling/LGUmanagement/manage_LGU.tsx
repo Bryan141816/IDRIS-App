37ea0dbbe2e72fa2073d5c5d288c50fdd8800074
Bryan141816 <bryanantier7@gmail.com>
2025-08-07 21:40:24 +0800

    update: all crud for evacuation center (Manage lgu)

--- IDRIS/src/pages/lgu_profiling/LGUmanagement/manage_LGU.tsx
+++ IDRIS/src/pages/lgu_profiling/LGUmanagement/manage_LGU.tsx
@@ -1,19 +1,53 @@
 import React, { useEffect, useState, useRef } from "react";
 import { Modal } from "../../../components/Page_Furniture/Modals";
 import "../css/LGUmanagement.css";
-import { Link } from "react-router-dom";
+import { data, Link } from "react-router-dom";
 import "../../response_dashboard/DefaultListViewStyle.scss";
 import { TableView } from "../../../components/TableView/table_view";
 import { API } from "../../../API_Handler/Axio_API_Handler";
 import { TableReponse } from "../../../components/TableView/table_view";
 import LocationPickerModal from "../../../components/Page_Furniture/LocationPickerModal";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faMapMarkerAlt } from "@fortawesome/free-solid-svg-icons";
+import {
+  faMapMarkerAlt,
+  faEllipsisVertical,
+  faTrash,
+  faPen,
+} from "@fortawesome/free-solid-svg-icons";
 import { MessageBox } from "../../../components/Page_Furniture/MessageBox";
+
+import { MapContainer, TileLayer, Marker } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+import L from "leaflet";
 //Mapview
+const defaultIcon = new L.Icon({
+  iconUrl: "https://unpkg.com/leaflet@1.9.3/dist/images/marker-icon.png",
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -34],
+  shadowUrl: "https://unpkg.com/leaflet@1.9.3/dist/images/marker-shadow.png",
+  shadowSize: [41, 41],
+});
 type Props = {
   lat: number;
   lng: number;
+};
+
+const MapWithPin: React.FC<Props> = ({ lat, lng }) => {
+  return (
+    <MapContainer
+      center={[lat, lng]}
+      zoom={13}
+      style={{ height: "100%", width: "100%" }}
+    >
+      <TileLayer
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        attribution="&copy; OpenStreetMap contributors"
+      />
+
+      <Marker position={[lat, lng]} icon={defaultIcon} />
+    </MapContainer>
+  );
 };
 import { faMapMarker } from "@fortawesome/free-solid-svg-icons";
 //API Handler
@@ -40,10 +74,13 @@ export async function addRecord(
   }
 }
 
-export async function deleteResponseReport(reportId: String): Promise<any> {
+export async function deleteRecord(
+  record_type: string,
+  reportId: String,
+): Promise<any> {
   try {
     const response = await API.delete(
-      `/response_dashboard/report_list/delete_report/${reportId}`,
+      `/lgu_profiling/manage_lgu/delete_${record_type}/${reportId}`,
     );
     return response;
   } catch (error: any) {
@@ -55,18 +92,15 @@ export async function deleteResponseReport(reportId: String): Promise<any> {
   }
 }
 
-export async function updateResponseReport(
+export async function updateRecord(
+  record_type: string,
   reportId: String,
-  report_type: String,
-  report_status: String,
+  payload: any,
 ) {
   try {
     const response = await API.put(
-      `/response_dashboard/report_list/update_report/${reportId}`,
-      {
-        report_type: report_type,
-        status: report_status,
-      },
+      `/lgu_profiling/manage_lgu/update_${record_type}/${reportId}`,
+      payload,
     );
     return { sucess: true, data: response.data };
   } catch (error: any) {
@@ -82,6 +116,13 @@ export async function updateResponseReport(
     }
   }
 }
+type MessageBoxState = {
+  isOpen: boolean;
+  type: "message" | "confirm";
+  message: string;
+  onSubmit?: () => void;
+  onClose: () => void;
+};
 type BaseModalProps = {
   isModalOpen: boolean;
   closeModal: () => void;
@@ -90,14 +131,21 @@ type BaseModalProps = {
 
 type addEvacuationModalProp = BaseModalProps & {
   handleAddEvacuation: (payload: any) => void;
-  defaultValue?: any;
+};
+type viewEvecuationModalProp = BaseModalProps & {
+  selectedData: any;
+  handleDeleteRecord: (id: number) => void;
+  openEditModal: () => void;
+};
+type editEvacuationModalProp = BaseModalProps & {
+  selectedData: any;
+  handleEditRecord: (id: string, payload: any) => void;
 };
 const AddEvacuationModal: React.FC<addEvacuationModalProp> = ({
   isModalOpen,
   closeModal,
   setMessageBox,
   handleAddEvacuation,
-  defaultValue,
 }) => {
   type evacuationProp = {
     name: string;
@@ -233,6 +281,259 @@ const AddEvacuationModal: React.FC<addEvacuationModalProp> = ({
     </>
   );
 };
+
+const ViewEvecuationModal: React.FC<viewEvecuationModalProp> = ({
+  isModalOpen,
+  closeModal,
+  setMessageBox,
+  selectedData,
+  handleDeleteRecord,
+  openEditModal,
+}) => {
+  const [isMoreOptionVisible, setIsMoreOptionVisible] = useState(false);
+  const toggleMoreOptionVisible = () => {
+    setIsMoreOptionVisible(!isMoreOptionVisible);
+  };
+  useEffect(() => {
+    if (!isModalOpen) {
+      setIsMoreOptionVisible(false);
+    }
+  }, [isModalOpen]);
+
+  return (
+    <Modal isOpen={isModalOpen} onClose={closeModal} zIndex={998}>
+      <div className="modal-container" style={{ paddingTop: "30px" }}>
+        <div className="horizontal-container space-between-container">
+          <span className="title-modal-text">View Details</span>
+          <div
+            className="horizontal-container"
+            style={{ width: "auto", gap: "5px" }}
+          >
+            <div className="more-options-container">
+              <button onClick={toggleMoreOptionVisible}>
+                <FontAwesomeIcon
+                  icon={faEllipsisVertical}
+                  style={{ height: "20px" }}
+                />
+              </button>
+              {isMoreOptionVisible && (
+                <div className="more-options-viewer">
+                  <button onClick={openEditModal}>
+                    <FontAwesomeIcon icon={faPen} />
+                    Edit Record
+                  </button>
+                  <button
+                    style={{ color: "red" }}
+                    onClick={() => {
+                      setMessageBox((prev) => ({
+                        ...prev, // preserves onClose and anything else
+                        isOpen: true, // your new values
+                        type: "confirm",
+                        message: "Are you sure you want to delete this record?",
+                        onSubmit: () => {
+                          handleDeleteRecord(selectedData.data[0].text);
+                        },
+                      }));
+                    }}
+                  >
+                    <FontAwesomeIcon icon={faTrash} /> Delete Record
+                  </button>
+                </div>
+              )}
+            </div>
+          </div>
+        </div>
+        <div className="horizontal-container">
+          <span className="details-title">Details</span>
+        </div>
+        <div className="horizontal-container">
+          <span className="item-details-identifier">Name:</span>
+          <span style={{ width: "100%", textAlign: "center" }}>
+            {selectedData.data[1].text}
+          </span>
+        </div>
+        <div className="horizontal-container">
+          <span className="item-details-identifier">
+            Location: (Latitude, Longitude)
+          </span>
+          <span style={{ width: "100%", textAlign: "center" }}>
+            {selectedData.data[2].text}, {selectedData.data[3].text}
+          </span>
+        </div>
+        <div
+          style={{
+            display: "flex",
+            width: "100%",
+            height: "50vh",
+            borderRadius: "10px",
+            overflow: "hidden",
+          }}
+        >
+          <MapWithPin
+            lat={parseFloat(selectedData.data[2].text)}
+            lng={parseFloat(selectedData.data[3].text)}
+          ></MapWithPin>
+        </div>
+        <div className="horizontal-container">
+          <span className="item-details-identifier">Capacity:</span>
+          <span style={{ width: "100%", textAlign: "center" }}>
+            {selectedData.data[4].text}
+          </span>
+        </div>
+        <div className="action-button">
+          <button style={{ backgroundColor: "#F84B4D" }} onClick={closeModal}>
+            Close
+          </button>
+        </div>
+      </div>
+    </Modal>
+  );
+};
+const EditEvacuationModal: React.FC<editEvacuationModalProp> = ({
+  isModalOpen,
+  closeModal,
+  setMessageBox,
+  handleEditRecord,
+  selectedData,
+}) => {
+  type evacuationProp = {
+    name: string;
+    lat: number;
+    lng: number;
+    capacity: number;
+  };
+  const [addEvacuationForm, setAddEvacuationForm] = useState<evacuationProp>({
+    name: selectedData.data[1].text,
+    lat: parseFloat(selectedData.data[2].text),
+    lng: parseFloat(selectedData.data[3].text),
+    capacity: parseInt(selectedData.data[4].text),
+  });
+  const [locationPickerIsOpen, setLocationPickerIsOpen] = useState(false);
+
+  const openLocationPicker = () => setLocationPickerIsOpen(true);
+  const closeLocationPicker = () => setLocationPickerIsOpen(false);
+  const handleLocationPickerSubmit = (mapData: {
+    lat: number;
+    lng: number;
+  }) => {
+    setAddEvacuationForm((prev) => ({
+      ...prev,
+      lat: mapData.lat,
+      lng: mapData.lng,
+    }));
+  };
+  const handleAddModalChange = (
+    e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>,
+  ) => {
+    const { name, value } = e.target;
+    setAddEvacuationForm((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+  return (
+    <>
+      {locationPickerIsOpen && (
+        <LocationPickerModal
+          isOpenProp={locationPickerIsOpen}
+          onCloseProp={closeLocationPicker}
+          onSubmit={handleLocationPickerSubmit}
+          lat={addEvacuationForm.lat}
+          lng={addEvacuationForm.lng}
+        />
+      )}
+      <Modal isOpen={isModalOpen} onClose={closeModal} zIndex={998}>
+        <div className="modal-container">
+          <div className="horizontal-container">
+            <span className="details-title">Update Evacuation</span>
+          </div>
+          <div className="horizontal-container">
+            <span className="item-details-identifier">Name:</span>
+            <input
+              type="text"
+              name="name"
+              value={addEvacuationForm.name}
+              onChange={handleAddModalChange}
+            />
+          </div>
+          <div className="horizontal-container">
+            <span className="item-details-identifier">Location:</span>
+            <div
+              style={{
+                display: "flex",
+                width: "100%",
+                flexDirection: "row",
+                gap: "5px",
+              }}
+            >
+              <input
+                type="text"
+                readOnly
+                placeholder="Select a location"
+                value={
+                  addEvacuationForm.lat
+                    ? `${addEvacuationForm.lat} , ${addEvacuationForm.lng}`
+                    : ""
+                }
+              />
+              <button
+                style={{
+                  backgroundColor: "transparent",
+                  border: "1px solid #ddd",
+                  outline: "none",
+                  color: "#3b82f6",
+                  width: "35px",
+                  borderRadius: "5px",
+                }}
+                onClick={openLocationPicker}
+              >
+                {" "}
+                <FontAwesomeIcon
+                  icon={faMapMarkerAlt}
+                  style={{ height: "20px" }}
+                />
+              </button>
+            </div>
+          </div>
+          <div className="horizontal-container">
+            <span className="item-details-identifier">Capacity:</span>
+            <input
+              type="number"
+              name="capacity"
+              value={addEvacuationForm.capacity}
+              onChange={handleAddModalChange}
+            />
+          </div>
+          <div className="action-button">
+            <button
+              style={{ backgroundColor: "#749AB6" }}
+              onClick={() => {
+                setMessageBox((prev) => ({
+                  ...prev, // preserves onClose and anything else
+                  isOpen: true, // your new values
+                  type: "confirm",
+                  message: "Are you sure you want to update this record?",
+                  onSubmit: () => {
+                    handleEditRecord(
+                      selectedData.data[0].text,
+                      addEvacuationForm,
+                    );
+                  },
+                }));
+              }}
+            >
+              Add
+            </button>
+            <button style={{ backgroundColor: "#F84B4D" }} onClick={closeModal}>
+              Cancel
+            </button>
+          </div>
+        </div>
+      </Modal>
+    </>
+  );
+};
+
 //
 
 const MapOfCebu = () => {
@@ -248,8 +549,8 @@ const MapOfCebu = () => {
   const [evacuationResponse, setEvacuationResponse] =
     useState<TableReponse | null>(null);
   const refreshTable = useRef<() => void>(() => {});
-  const [currentTabName, setCurrentTabName] = useState("LGU");
   const handleRefreshTable = () => refreshTable.current?.();
+  const [selectedViewData, setSelectedViewData] = useState<any>(null);
   type MessageBoxState = {
     isOpen: boolean;
     type: "message" | "confirm";
@@ -295,6 +596,32 @@ const MapOfCebu = () => {
     hazard: false,
     evacuation: false,
   });
+  const [viewModalState, setViewModalState] = useState<{
+    lguModal: boolean;
+    barangay: boolean;
+    rafi: boolean;
+    hazard: boolean;
+    evacuation: boolean;
+  }>({
+    lguModal: false,
+    barangay: false,
+    rafi: false,
+    hazard: false,
+    evacuation: false,
+  });
+  const [editModalState, setEditModalState] = useState<{
+    lguModal: boolean;
+    barangay: boolean;
+    rafi: boolean;
+    hazard: boolean;
+    evacuation: boolean;
+  }>({
+    lguModal: false,
+    barangay: false,
+    rafi: false,
+    hazard: false,
+    evacuation: false,
+  });
 
   const openAddModal = () => {
     setAddModalState((prev) => ({
@@ -307,6 +634,44 @@ const MapOfCebu = () => {
       ...prev,
       [activeTab]: false,
     }));
+  };
+  const openViewModal = () => {
+    setViewModalState((prev) => ({
+      ...prev,
+      [activeTab]: true,
+    }));
+  };
+  const closeViewModal = () => {
+    setViewModalState((prev) => ({
+      ...prev,
+      [activeTab]: false,
+    }));
+  };
+  const openEditModal = () => {
+    closeViewModal();
+    setEditModalState((prev) => ({
+      ...prev,
+      [activeTab]: true,
+    }));
+  };
+  const closeEditModal = () => {
+    setEditModalState((prev) => ({
+      ...prev,
+      [activeTab]: false,
+    }));
+    openViewModal();
+  };
+
+  const handleDeleteRecord = async (id: Number) => {
+    const response = await deleteRecord(activeTab, id);
+    setMessageBox((prev) => ({
+      ...prev, // preserves onClose and anything else
+      isOpen: true, // your new values
+      type: "message",
+      message: "Record deleted successfully",
+    }));
+    closeViewModal();
+    handleRefreshTable();
   };
 
   useEffect(() => {
@@ -331,6 +696,28 @@ const MapOfCebu = () => {
         message: "Record failed",
       }));
     }
+  };
+  const handleEditRecord = async (id: string, payload: any) => {
+    const response = await updateRecord(activeTab, id, payload);
+    setMessageBox((prev) => ({
+      ...prev, // preserves onClose and anything else
+      isOpen: true, // your new values
+      type: "message",
+      message: "Record has been updated",
+    }));
+    setSelectedViewData((prev: any) => {
+      const newData = [...prev.data];
+      newData[1].text = payload.name;
+      newData[2].text = payload.lat;
+      newData[3].text = payload.lng;
+      newData[4].text = payload.capacity;
+      return {
+        ...prev,
+        data: newData,
+      };
+    });
+    closeEditModal();
+    openViewModal();
   };
   const closeMessageBox = () => {
     setMessageBox((prev) => ({
@@ -361,6 +748,25 @@ const MapOfCebu = () => {
         setMessageBox={setMessageBox}
         handleAddEvacuation={handleAddRecord}
       ></AddEvacuationModal>
+      {selectedViewData && (
+        <ViewEvecuationModal
+          isModalOpen={viewModalState.evacuation}
+          closeModal={closeViewModal}
+          setMessageBox={setMessageBox}
+          selectedData={selectedViewData}
+          handleDeleteRecord={handleDeleteRecord}
+          openEditModal={openEditModal}
+        ></ViewEvecuationModal>
+      )}
+      {selectedViewData && (
+        <EditEvacuationModal
+          isModalOpen={editModalState.evacuation}
+          closeModal={closeEditModal}
+          setMessageBox={setMessageBox}
+          selectedData={selectedViewData}
+          handleEditRecord={handleEditRecord}
+        ></EditEvacuationModal>
+      )}
 
       <div className="tabs">
         <button
@@ -496,7 +902,10 @@ const MapOfCebu = () => {
             {evacuationResponse ? (
               <TableView
                 tableJSON={evacuationResponse}
-                onClickCallback={(row: any) => {}}
+                onClickCallback={(row: any) => {
+                  setSelectedViewData(row);
+                  openViewModal();
+                }}
                 setCallbackTableData={true}
                 pageRequest={`/lgu_profiling/manage_lgu/get_evacuation?page=`}
                 updateTable={(fn) => (refreshTable.current = fn)}
--- backend/routers/lgu_profiling/manage_lgu.py
+++ backend/routers/lgu_profiling/manage_lgu.py
@@ -1,4 +1,4 @@
-from crud import delete
+from crud import delete, update
 from fastapi import APIRouter, Query
 from fastapi import Depends, HTTPException
 from sqlalchemy.orm import Session
@@ -21,7 +21,7 @@ def getDefaultPage(page):
 
 
 @router.get("/lgu_profiling/manage_lgu/get_lgu", response_model=TableResponse)
-def get_lgu(db: Session = Depends(get_db), page: int = Query(1, ge=1), Name="desci"):
+def get_lgu(db: Session = Depends(get_db), page: int = Query(1, ge=1), Name="desc"):
     table_head = [
         {"text": "Name", "width": "150px", "action": "Sort"},
         {"text": "Lat", "width": "150px"},
@@ -185,135 +185,34 @@ def add_evacuation(record: EvacuationCenterCreate, db: Session = Depends(get_db)
     return db_record
 
 
-#
-# def getDefaultPage(page):
-#     return math.floor((page - 1) / 100) * 100 + 1
-#
-#
-# @router.get("/response_dashboard/budget_record/get_list", response_model=TableResponse)
-# def get_budget_table(
-#     db: Session = Depends(get_db), page: int = Query(1, ge=1), Date: str = "desc"
-# ):
-#     # Table header remains the same
-#
-#     page = getDefaultPage(page)
-#
-#     offset = (page - 1) * 10
-#
-#   table_head = [
-#         {"text": "Date", "width": "150px", "action": "Sort"},
-#         {"text": "Record Type", "width": "250px"},
-#         {"text": "Amount", "width": "150px"},
-#         {"text": "Total Amount", "width": "150px"},
-#         {"text": "Actions", "width": "150px"},
-#     ]
-#
-#     order = (
-#         ResponseReportBudget.date_time.desc()
-#         if Date == "desc"
-#         else ResponseReportBudget.date_time.asc()
-#     )
-#     # Query all reports (limit if needed)
-#     reports = (
-#         db.query(ResponseReportBudget).order_by(order).limit(100).offset(offset).all()
-#     )
-#
-#     table_datas = []
-#
-#     pageCount = page
-#     pages = {"page": pageCount, "row": []}
-#     for report in reports:
-#         if len(pages["row"]) == 10:
-#             table_datas.append(pages)
-#             pageCount += 1
-#             pages = {"page": pageCount, "row": []}
-#         row_data = [
-#             Cell(
-#                 type="Hidden",  # Custom type handled in frontend
-#                 text=str(report.id),
-#                 font_weight=0,
-#                 color="#000",
-#                 width="0px",
-#             ),
-#             Cell(
-#                 type="Text",
-#                 text=report.date_time.strftime("%B %d, %Y"),  # format date nicely
-#                 font_weight=500,
-#                 color="#000",
-#                 width="150px",
-#             ),
-#             Cell(
-#                 type="Text",
-#                 text=report.budget_record_type,
-#                 font_weight=500,
-#                 color="#000",
-#                 width="250px",
-#             ),
-#             Cell(
-#                 type="Text",
-#                 text=str(report.amount),
-#                 font_weight=700,
-#                 color="#000",  # color green if completed
-#                 width="150px",
-#             ),
-#             Cell(
-#                 type="Text",
-#                 text=str(report.total_amount),
-#                 font_weight=700,
-#                 color="#000",
-#                 width="150px",
-#             ),
-#             Cell(
-#                 type="Button",
-#                 text="View",
-#                 font_weight=500,
-#                 color="#fff",
-#                 background_color="#749AB6",
-#                 container_width="150px",
-#                 button_width="120px",
-#             ),
-#         ]
-#         pages["row"].append({"data": row_data})
-#     if pages["row"]:
-#         table_datas.append(pages)
-#     count = db.query(ResponseReportBudget).count()
-#     return TableResponse(table_head=table_head, table_datas=table_datas, count=count)
-#
-#
-# @router.post(
-#     "/response_dashboard/budget_record/add_record",
-#     response_model=ResponseDashboardBudgetOut,
-# )
-# def add_budget_record(
-#     record: ResponseDashboardBudgetCreate, db: Session = Depends(get_db)
-# ):
-#     return create_response_dashboard_budget_create(db, record)
-#
-#
-# @router.delete(
-#     "/response_dashboard/budget_record/delete_record/{record_id}", response_model=dict
-# )
-# def delete_budget_record(record_id: int, db: Session = Depends(get_db)):
-#     deleted_report = delete(db, ResponseReportBudget, record_id)
-#     if not deleted_report:
-#         raise HTTPException(status_code=400, detail="Response report not found.")
-#     return {"message": f"Record with ID {record_id} deleted successfully."}
-#
-#
-# @router.put("/response_dashboard/budget_record/update_record/{record_id}")
-# def update_budget_record(
-#     record_id: int, update: ResponseDashboardBudgetCreate, db: Session = Depends(get_db)
-# ):
-#     record = db.query(ResponseReportBudget).get(record_id)
-#
-#     if not record:
-#         raise HTTPException(status_code=404, detail="Response record doesn't exist")
-#     if update.budget_record_type is not None:
-#         record.budget_record_type = update.budget_record_type
-#     if update.amount is not None:
-#         record.amount = update.amount
-#
-#     db.commit()
-#     db.refresh(record)
-#
-#     return {"detail": "Record updated succesfully", "record": record}
+@router.delete(
+    "/lgu_profiling/manage_lgu/delete_evacuation/{record_id}", response_model=dict
+)
+def delete_budget_record(record_id: int, db: Session = Depends(get_db)):
+    deleted_report = delete(db, EvacuationCenter, record_id)
+    if not deleted_report:
+        raise HTTPException(status_code=400, detail="Record not found.")
+    return {"message": f"Record with ID {record_id} deleted successfully."}
+
+
+@router.put("/lgu_profiling/manage_lgu/update_evacuation/{record_id}")
+def update_budget_record(
+    record_id: int, payload: EvacuationCenterCreate, db: Session = Depends(get_db)
+):
+    record = db.query(EvacuationCenter).get(record_id)
+
+    if not record:
+        raise HTTPException(status_code=404, detail="Response record doesn't exist")
+    if payload.name is not None:
+        record.name = payload.name
+    if payload.lat is not None:
+        record.lat = payload.lat
+    if payload.lng is not None:
+        record.lng = payload.lng
+    if payload.capacity is not None:
+        record.capacity = payload.capacity
+
+    db.commit()
+    db.refresh(record)
+
+    return {"detail": "Record updated succesfully", "record": record}

92e69faf6260c1688822eb3e02915348d2a41795
Bryan141816 <bryanantier7@gmail.com>
2025-10-10 16:40:21 +0800

    fix: Accept all routes if account role is super admin

--- backend/routers/role_checker.py
+++ backend/routers/role_checker.py
@@ -10,11 +10,15 @@ def RoleChecker(required_roles: List[str]):
     def checker(
         current_user: User = Depends(get_current_user_from_access_token),
     ):
+        if "super admin" in current_user.roles:
+            return current_user
+
         if not any(role in current_user.roles for role in required_roles):
             raise HTTPException(
                 status_code=status.HTTP_403_FORBIDDEN,
                 detail="User does not have the required role",
             )
+
         return current_user
 
     return checker

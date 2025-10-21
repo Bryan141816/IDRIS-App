36de36e1de5480c7c61f56101ced538bc6c02bb5
Christy Montejo <christymontejo2003@gmail.com>
2025-10-22 02:46:05 +0800

    removed avg response time card

--- IDRIS/src/pages/response_dashboard/ResponseDashboard.tsx
+++ IDRIS/src/pages/response_dashboard/ResponseDashboard.tsx
@@ -1012,7 +1012,7 @@ const ResponseDashboard = () => {
             </div>
 
             {/* Response Time - Critical for Operations */}
-            <div className="sub-item-content-big-data-inverted">
+            {/* <div className="sub-item-content-big-data-inverted">
               <h1>Avg Response Time</h1>
               <div className="horizontal-container full-width space-between-container">
                 {reportSummary ? (
@@ -1038,7 +1038,7 @@ const ResponseDashboard = () => {
                   Target: ≤ 2h
                 </span>
               </div>
-            </div>
+            </div> */}
 
             {/* Staff Status - Enhanced with better spacing */}
             <div className="sub-item-content-big-data-inverted">

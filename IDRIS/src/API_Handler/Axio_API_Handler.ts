19c322859bba3a5431a273e593aa5a61c3352ece
Bryan141816 <bryanantier7@gmail.com>
2025-11-12 05:39:50 +0800

    update: Added fallback for api

--- IDRIS/src/API_Handler/Axio_API_Handler.ts
+++ IDRIS/src/API_Handler/Axio_API_Handler.ts
@@ -28,7 +28,7 @@ function isTokenExpired(token: string) {
 }
 
 export const API = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL: import.meta.env.VITE_API_URL || "http://localhost:8000",
   withCredentials: true,
 });
 
